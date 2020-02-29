8b5b9d3145f404108d860b30f409eedeccb424b3
xinkuiwu <1079894635@qq.com>
2020-02-29 11:17:51 +0800

    Site updated: 2020-02-29 11:17:45

--- js/src/fold.js
+++ js/src/fold.js
@@ -1,3 +1,3 @@
-// build time:Sat Feb 29 2020 11:09:00 GMT+0800 (GMT+08:00)
+// build time:Sat Feb 29 2020 11:17:43 GMT+0800 (GMT+08:00)
 function fold(d,e){var i=d[0];if(!i)i="点击显/隐";return'<div><div class="fold_hider"><div class="close hider_title">'+i+'</div></div><div class="fold">\n'+hexo.render.renderSync({text:e,engine:"markdown"})+"\n</div></div>"}hexo.extend.tag.register("fold",fold,{ends:true});
 //rebuild by neat 
